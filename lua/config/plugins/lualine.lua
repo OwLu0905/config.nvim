ea2cbbec40a8c8a9c4447e2110d78a85d3f0856d
owlu0905 <owlu0905@gmail.com>
2025-04-16 00:44:35 +0800

    feat: lualine mode style

--- lua/config/plugins/lualine.lua
+++ lua/config/plugins/lualine.lua
@@ -3,5 +3,19 @@ return {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {},
+    config = function()
+      require("lualine").setup {
+        sections = {
+          lualine_a = {
+            {
+              "mode",
+              fmt = function(str)
+                return str:sub(1, 1)
+              end,
+            },
+          },
+        },
+      }
+    end,
   },
 }
