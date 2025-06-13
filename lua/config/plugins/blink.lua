33838df0e25b70a8b90455f47f5a100abf0a9871
owlu0905 <owlu0905@gmail.com>
2025-06-13 15:14:38 +0800

    chore: update blink & lsp setting

--- lua/config/plugins/blink.lua
+++ lua/config/plugins/blink.lua
@@ -1,76 +1,22 @@
--- NOTE: https://www.reddit.com/r/neovim/comments/1i06ajo/comment/m73cy5z/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
-
 return {
   {
     "saghen/blink.cmp",
-    enabled = true,
-    version = "*",
-    dependencies = {
-      "mikavilpas/blink-ripgrep.nvim",
-      {
-        "L3MON4D3/LuaSnip",
-        version = "v2.*",
-        build = "make install_jsregexp",
-        dependencies = {
-          "rafamadriz/friendly-snippets",
-          config = function()
-            require("luasnip.loaders.from_vscode").lazy_load()
-            require("luasnip.loaders.from_vscode").lazy_load {
-              paths = { vim.fn.stdpath "config" .. "/snippets" },
-            }
-
-            local extends = {
-              typescript = { "tsdoc" },
-              javascript = { "jsdoc" },
-              lua = { "luadoc" },
-              python = { "pydoc" },
-              rust = { "rustdoc" },
-              cs = { "csharpdoc" },
-              java = { "javadoc" },
-              c = { "cdoc" },
-              cpp = { "cppdoc" },
-              php = { "phpdoc" },
-              kotlin = { "kdoc" },
-              ruby = { "rdoc" },
-              sh = { "shelldoc" },
-            }
-            -- friendly-snippets - enable standardized comments snippets
-            for ft, snips in pairs(extends) do
-              require("luasnip").filetype_extend(ft, snips)
-            end
-          end,
-        },
-        opts = { history = true, delete_check_events = "TextChanged" },
-      },
-    },
+    dependencies = { "L3MON4D3/LuaSnip", version = "v2.*" },
+    version = "1.*",
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
       snippets = { preset = "luasnip" },
-      completion = { documentation = { auto_show = false } },
-      sources = {
-        default = {
-          "lsp",
-          "path",
-          "buffer",
-          "snippets",
-          -- 'ripgrep',
-        },
-        --  providers = {
-        --    ripgrep = {
-        --      module = 'blink-ripgrep',
-        --      name = 'Ripgrep',
-        --      ---@module "blink-ripgrep"
-        --      ---@type blink-ripgrep.Options
-        --      opts = {
-        --        prefix_min_len = 4,
-        --        score_offset = 10, -- should be lower priority
-        --        max_filesize = '300K',
-        --        search_casing = '--smart-case',
-        --      },
-        --    },
-        -- }
+      keymap = { preset = "default" },
+      appearance = {
+        nerd_font_variant = "mono",
       },
+      completion = { documentation = { auto_show = true } },
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+      },
+      fuzzy = { implementation = "prefer_rust_with_warning" },
     },
+    opts_extend = { "sources.default" },
   },
 }
--- lua/config/plugins/lsp/init.lua
+++ lua/config/plugins/lsp/init.lua
@@ -11,15 +11,23 @@ return {
         ensure_installed = { "lua_ls", "rust_analyzer" },
       }
 
-      local lspconfig = require "lspconfig"
+      vim.lsp.enable "lua_ls"
+      vim.lsp.enable "ts_ls"
+      vim.lsp.enable "svelte"
+      vim.lsp.enable "tailwindcss"
+      vim.lsp.enable "ruff"
+      vim.lsp.enable "pyright"
+      vim.lsp.enable "rust_analyzer"
 
-      lspconfig.lua_ls.setup {}
-      lspconfig.ts_ls.setup {}
-      lspconfig.svelte.setup {}
-      lspconfig.tailwindcss.setup {}
-      lspconfig.ruff.setup {}
-      lspconfig.pyright.setup {}
-      lspconfig.rust_analyzer.setup {}
+      -- local lspconfig = require "lspconfig"
+      --
+      -- lspconfig.lua_ls.setup {}
+      -- lspconfig.ts_ls.setup {}
+      -- lspconfig.svelte.setup {}
+      -- lspconfig.tailwindcss.setup {}
+      -- lspconfig.ruff.setup {}
+      -- lspconfig.pyright.setup {}
+      -- lspconfig.rust_analyzer.setup {}
 
       vim.diagnostic.config {
         virtual_lines = false,
